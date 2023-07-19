d13282691544f9fd14c6f295909d7bfbc5bf739b
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-19 09:14:38 +0330

    add category

--- src/components/modals/categoryModal.js
+++ src/components/modals/categoryModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState } from 'react'
 import {
   ListItem,
   OrderedList, Modal,
@@ -7,45 +7,60 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton, Button, Input, Select
+  ModalCloseButton, Button, Input, Select, FormControl
 } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+
 const CategoryModal = (props) => {
 
+  const [ category , setCategory  ] = useState([])
 
+  const {register , handleSubmit } = useForm()
 
   const renderedCategoryList = (category , categoryList=[]) => {
    
-    // for (let cat of category) {
-    //   categoryList.push(cat.name)
-    //   {  renderedCategoryList(cat , categoryList) }
-    // }
+    for (let cat of category) {
+      categoryList.push(cat.name)
+      {  renderedCategoryList(cat.children , categoryList) }
+    }
     return categoryList
   }
-  const catList = renderedCategoryList(props.category)
 
- 
+  useEffect(()=>{
+    setCategory(renderedCategoryList(props.category))
+  },[])
+
+ const handleSendForm = (value)=>{
+  console.log(value.file[0].name , value.title , value.parent)
+  
+ }
 
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose} >
       <ModalOverlay />
-      <ModalContent dir='rtl'>
+      <ModalContent dir='rtl' >
         <ModalHeader mr={'50px'}>افزودن عنوان</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <Input placeholder='نام عنوان' size={'sm'} />
-          <Select mt={4}>
-
+        <ModalBody >
+          <form onSubmit={handleSubmit(handleSendForm)}>
+          <Input placeholder='نام عنوان' size={'sm'} {...register('title')}/>
+          <Select as={'select'} mt={4} dir='ltr' {...register('parent')}>
+           { category.map((item)=>
+             ( <option value={item}>{item}</option>)
+            ) }
           </Select>
+          <Input type='file' mt={4} {...register('file')}/>
+          <Button variant='ghost' border={'1px'} borderColor={'blackAlpha.300'} _hover={{
+            bg: 'cyan.400',
+            color: 'white',
+          }} type='submit' mt={'40px'}>افزودن</Button>
+          </form>
         </ModalBody>
 
         <ModalFooter>
           {/* <Button colorScheme='blue' mr={3} onClick={props.onClose}>
             Close
           </Button> */}
-          <Button variant='ghost' border={'1px'} borderColor={'blackAlpha.300'} _hover={{
-            bg: 'cyan.400',
-            color: 'white',
-          }}>افزودن</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
