624cbdda943b8958f8e65c08f08ffc68e4c53847
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-22 10:36:38 +0330

    add dispatch initial

--- src/redux/actions/initialData.js
+++ src/redux/actions/initialData.js
@@ -3,12 +3,11 @@ import { initialDataConstants } from "./constants"
 
 export const initialData = () =>async(dispatch)=>{
   try{
-
     dispatch({type:initialDataConstants.GET_ALL_PRODUCT_REQUEST})
     const res = await axiosInstance.get('/admin/initialdata')
-    const {product , catgory} = res.data
+    const {product , category} = res.data
     dispatch({type:initialDataConstants.GET_ALL_PRODUCT_SUCCESS , payload:product})
+    dispatch({type:initialDataConstants.GET_ALL_CATEGORY_SUCCESS , payload:category})
 
-
-  }catch(err){dispatch()}
+  }catch(err){dispatch({type:initialDataConstants.GET_ALL_PRODUCT_FAILURE ,payload:{error:err.message}})}
 }
--- src/redux/reducers/product.js
+++ src/redux/reducers/product.js
@@ -1,4 +1,4 @@
-import { productConstants } from "../actions/constants";
+import { initialDataConstants, productConstants } from "../actions/constants";
 
 const initialState = {
   loading : false , 
@@ -7,14 +7,14 @@ const initialState = {
 
 export const product = ( state=initialState, action)=>{
   switch(action.type){
-    case productConstants.GET_PRODUCT_REQUEST:
+    case initialDataConstants.GET_ALL_PRODUCT_REQUEST:
       return {
         ...state
       }
-      case productConstants.GET_PRODUCT_SUCCESS:
-        return {
-          ...state , products:action.payload
-        }
+        case initialDataConstants.GET_ALL_PRODUCT_SUCCESS:
+          return {
+            ...state , products:action.payload
+          }
         default:
           return {
             ...state
